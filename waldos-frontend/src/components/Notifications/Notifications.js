235380c7e576d88e1d82d7c0155662e9ec4f83c9
RatulPradhan <ratulrpradhan@gmail.com>
2024-10-20 14:59:23 -0400

    Adding frontend

---
+++ waldos-frontend/src/components/Notifications/Notifications.js
@@ -0,0 +1,10 @@
+import { Box, Flex, Text, Button, VStack, Avatar } from "@chakra-ui/react";
+import Sidebar from "../Navbar/Sidebar";
+
+export default function Notifications() {
+  return (
+  <Flex height="100vh">
+    <Sidebar />
+  </Flex>
+  );
+};
